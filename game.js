f1b47ac19b329fdf6e645a76d4c7849069d8eab7
Hunter-Mayer <hmayer95@gmail.com>
2022-09-12 19:37:15 -0500

    pseudo code

--- game.js
+++ game.js
@@ -1,1 +1,29 @@
-console.log('Yo')
+//Pseudo Code Outline for RPS
+
+//define array for  choice of rock paper or scissors [r, p, s]
+
+//define var for human choice 
+
+//define var for computer choice
+
+//define var for human wins
+
+//define var for human ties
+
+//define var for human loses 
+
+//prompt (use method) humanChoice
+
+//randomly choose computer choice
+
+//compare choices of human and computer
+    //logic for comparing choices here
+
+//alert player of comparison result (won, tied, lost round)
+
+//alert player of stats (total wins, loses, ties)
+
+//ask to play again?
+    //restart game
+//else 
+    //stop the game
